57d4c7195ebb1b4e73999ee9d5c56158dfc06cf6
Walter <relw1wa-github@yahoo.de>
2017-08-21 20:26:20 +0200

    Use Sinon to test click event handling

--- test/pomodoro-controls.spec.js
+++ test/pomodoro-controls.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
+import { spy } from 'sinon';
 
 import PomodoroControls from '../src/components/pomodoro-controls';
 
@@ -16,6 +17,20 @@ describe('<PomodoroControls />', function () {
     const wrapper = shallow(<PomodoroControls pomodoroStarted={true} />);
     expect(wrapper.find('button.pomodoro-start')).to.have.length(0);
     expect(wrapper.find('button.pomodoro-stop')).to.have.length(1);    
+  });
+
+  it('\'s start button should call onStartPomodoro when clicked', function() {
+    const onStartPomodoroSpy = spy();
+    const wrapper = shallow(<PomodoroControls pomodoroStarted={false} onStartPomodoro={onStartPomodoroSpy}/>);
+    wrapper.find('.pomodoro-start').simulate('click');
+    expect(onStartPomodoroSpy.calledOnce).to.equal(true);
+  });
+
+  it('\'s stop button should call onStopPomodoro when clicked', function() {
+    const onStopPomodoroSpy = spy();
+    const wrapper = shallow(<PomodoroControls pomodoroStarted={true} onStopPomodoro={onStopPomodoroSpy}/>);
+    wrapper.find('.pomodoro-stop').simulate('click');
+    expect(onStopPomodoroSpy.calledOnce).to.equal(true);
   });
 
 });
